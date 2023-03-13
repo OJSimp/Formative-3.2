9855aaf5ceb1ebc20c84309711ed73a2a418e963
OJSimp <spencersimpson332@gmail.com>
2023-03-13 16:09:11 +1300

    add border box as box sizing to app.js

--- front/src/components/Nav.js
+++ front/src/components/Nav.js
@@ -6,9 +6,9 @@ import { Link } from "react-router-dom"
 const Nav = () => {
 
  return(
-  <nav className="nav-menu">
+ <nav className="nav-menu">
    <ul>
-    <li><Link to="/" className="nav-button">Home</Link></li>
+    <li><Link to="/" className="nav-button">Feed <i className="nav-icon"></i></Link></li>
     <li><Link to="/search" className="nav-button">Search</Link></li>
     <li><Link to="/post" className="nav-button">Post</Link></li>
    </ul>
