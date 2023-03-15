51ce4bf8a5ea12f09c675eb9c3cef5656778067c
ZeeshanAliii <317Zeeali@gmail.com>
2023-03-16 09:30:55 +1300

    updated the html

--- front/src/pages/Search.js
+++ front/src/pages/Search.js
@@ -1,40 +1,62 @@
-import "./Search.scss"
+import "./Search.scss";
 
-import Nav from "../components/Nav"
+import Nav from "../components/Nav";
 
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
-import { useState } from "react"
+import { useState } from "react";
 
 const Search = () => {
+  const [searchValue, setSearchValue] = useState("");
 
-const [searchValue, setSearchValue] = useState("")
+  const handleSearchInput = (e) => {
+    e.preventDefault();
 
-const handleSearchInput = (e) => {
-e.preventDefault()
+    console.log(searchValue);
+  };
 
-console.log(searchValue)
-}
+  const handleSearchValue = (e) => [setSearchValue(e.target.value)];
 
- const handleSearchValue = (e) => [
-
- setSearchValue(e.target.value)
-
-]
-
- return(
-  <div className="search-page">
-    <div className="search-header">
-      <form action="" className="search-from" onSubmit={handleSearchInput}>
-        <input type="text" className="search-input" placeholder="search here for" onChange={handleSearchValue}/>
-        <Link to={`/${searchValue}`} className="primary-button">Search</Link>
-      </form>
+  return (
+    <div className="search-page">
+      <div className="search-header">
+        <form action="" className="search-from" onSubmit={handleSearchInput}>
+          <input
+            type="text"
+            className="search-input"
+            placeholder="search here for"
+            onChange={handleSearchValue}
+          />
+          <Link to={`/${searchValue}`} className="primary-button">
+            Search
+          </Link>
+        </form>
+        <div className="search-photos-grid">
+          <div className="photocontainertop">
+            <img
+              className="topimg"
+              src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+              alt=""
+            ></img>
+          </div>
+          <div className="photocontainermiddle">
+            <img
+              className="middleimg"
+              src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+              alt=""
+            ></img>
+            <img
+              className="middleimg"
+              src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+              alt=""
+            ></img>
+            <div className="photocontainerbottom"></div>
+          </div>
+        </div>
+      </div>
+      <Nav />
     </div>
-   < Nav />
-  </div>
-  
-  )
+  );
+};
 
-}
-
-export default Search
+export default Search;
